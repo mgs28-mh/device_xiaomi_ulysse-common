4f56f234b30e5ae20deb502b839d957ac11899c6
xyyx <xyyx@mail.ru>
2020-07-28 13:27:51 +0530

    ulysse-common: Add basic USB HAL that reports no status change
    Change-Id: I46874b1d6a6491d07b7d5ae896a192312b4047cd

--- ulysse.mk
+++ ulysse.mk
@@ -492,7 +492,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.ulysse
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
