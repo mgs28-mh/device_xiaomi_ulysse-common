435a049e67986e495d38a1a906ab42f2a9388b45
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    power: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- ulysse.mk
+++ ulysse.mk
@@ -368,7 +368,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.onclite-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
 
 PRODUCT_COPY_FILES += \
