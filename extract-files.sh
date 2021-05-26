e9b3d668a0b690a3a437eb52a5e9414bb0a065cd
garisku05 <garisku05@gmail.com>
2021-05-26 18:20:04 +0700

    ulysse-common: move most blobs to proprietary-files.txt

--- extract-files.sh
+++ extract-files.sh
@@ -8,13 +8,16 @@
 
 set -e
 
+DEVICE_COMMON=ulysse-common
+VENDOR=xiaomi
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="../../../extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
