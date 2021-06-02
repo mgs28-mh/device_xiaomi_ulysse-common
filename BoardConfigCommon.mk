c251cd9cf4f1b5120cedc70f268603db3822b03d
garisku05 <garisku05@gmail.com>
2021-06-02 22:14:30 +0700

    ulysse-common: Set TARGET_COMPILE_WITH_MSM_KERNEL to true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,25 +45,19 @@ BUILD_BROKEN_PREBUILT_ELF_FILES := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 ignore_loglevel androidboot.console=ttyHSL0
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.usbconfigfs=true
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-BOARD_KERNEL_PAGESIZE :=  2048
+BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := ulysse_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ulysse
 TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
-ifeq ($(TARGET_KERNEL_VERSION),4.9)
-BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
 TARGET_KERNEL_CLANG_COMPILE := true
-else ifeq ($(TARGET_KERNEL_VERSION),3.18)
-TARGET_KERNEL_ARCH := arm64
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-endif
-ifneq ($(wildcard vendor/qcom/sdclang-8.0/linux-x86_64),)
-TARGET_KERNEL_CLANG_PATH := $(PWD)/vendor/qcom/sdclang-8.0/linux-x86_64
-endif
-TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_COMPILE_WITH_MSM_KERNEL	:= true
+TARGET_KERNEL_CLANG_VERSION := proton-clang
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
