aaaf344d54d22da18362854dc7f6920f12acfc88
mgs28-mh <gsaputra880@gmail.com>
2021-06-06 09:21:24 +0700

    ulysse-common: Drop duplicate telephony-ext jar defination
    Change-Id: Ia96e9dd3774e13200dfcc92ed763f53048c5c86a

--- ulysse.mk
+++ ulysse.mk
@@ -406,9 +406,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
