f5d466e2f3300bc93afada042217544f9a3c81a6
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-07-30 13:29:48 +0700

    ulysse-common: Update ADSP and DSP blobs from LA.UM.9.6.2.r1-03600-89xx.0
    * Move vendor.qti.hardware.dsp.policy to vendor
    
    Signed-off-by: mgs28-mh <gsaputra880@gmail.com>

--- ulysse.mk
+++ ulysse.mk
@@ -469,7 +469,6 @@ PRODUCT_BOOT_JARS += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
