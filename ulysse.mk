5d5ca0ed9fbf655c635972981c6f033edcf0bd53
Quallenauge <Hamsi2k@freenet.de>
2021-09-15 19:40:28 +0100

    power: Rebrand xiaomi -> xiaomi_msm8937.
    Change-Id: I3cf39b3ce4d3ed7f35651b6268c3dcb67bd9e8db

--- ulysse.mk
+++ ulysse.mk
@@ -376,7 +376,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.xiaomi_msm8937-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
 
 PRODUCT_COPY_FILES += \
