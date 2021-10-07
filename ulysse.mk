f5bc4971ed50fbcb6f3ec61013cee8c5c044f39c
mgs28-mh <gsaputra880@gmail.com>
2021-10-07 08:55:49 +0700

    ulysse-common: drop QCOM WFD
    Change-Id: Ied6805a05dd002f7d6b0974c72d14c5d4d9e046c

--- ulysse.mk
+++ ulysse.mk
@@ -85,7 +85,6 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libaudioroute \
-    libaacwrapper \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
@@ -538,15 +537,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# Wi-Fi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libaacwrapper \
-    libwfdaac
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
