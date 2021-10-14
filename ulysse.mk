e8ccf2649ae76f6db654fdbe39171d6ee2eed3c7
mgs28-mh <gsaputra880@gmail.com>
2021-10-14 17:42:59 +0700

    ulysse-common: Build libtiny from source
    Change-Id: If884817dba8eb8a903332a3919013c00c4447fe1

--- ulysse.mk
+++ ulysse.mk
@@ -520,6 +520,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# TinyXML
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
