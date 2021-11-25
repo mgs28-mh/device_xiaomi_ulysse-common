557a1a74003a0ddae2a470cd0dbffd9ed7540d90
Chenyang Zhong <zhongcy95@gmail.com>
2021-11-25 06:29:59 +0000

    ulysse-common: sepolicy: Address init denial
    Change-Id: Id0d0c88bbedde6b6586e3a6f04704457d910d8a0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,6 +179,7 @@ VENDOR_SECURITY_PATCH := 2020-03-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
