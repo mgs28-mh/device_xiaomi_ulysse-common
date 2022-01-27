a13dd0ebbcb307c7b2089d89bee6ab7697e2b01c
mgs28-mh <gsaputra880@gmail.com>
2022-01-27 10:11:41 +0700

    ulysse-common: clean blobs and build from source
    Change-Id: I9f3f03a20b6cddcec147c2f5349676dc846b8591

--- ulysse.mk
+++ ulysse.mk
@@ -157,6 +157,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
