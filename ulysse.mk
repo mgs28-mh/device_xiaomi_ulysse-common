4e1700ed705401143379175738730846fdf6e7fc
Ramakant Singh <ramakant@codeaurora.org>
2022-02-27 21:12:04 +0100

    ulysse-common: Remove redundant lib displayconfig
    Change-Id: I316685149b630a19d489350540a35611c711e5a0

--- ulysse.mk
+++ ulysse.mk
@@ -187,8 +187,6 @@ PRODUCT_PACKAGES += \
     gralloc.msm8937 \
     hwcomposer.msm8937 \
     memtrack.msm8937 \
-    libdisplayconfig \
-    libdisplayconfig.qti \
     libqdMetaData.vendor \
     liboverlay \
     libqdMetaData \
