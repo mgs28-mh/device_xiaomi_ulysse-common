f6b8f6a18d7fd6d0f20849d277954fa6eae37fb4
mgs28-mh <gsaputra880@gmail.com>
2022-04-10 06:44:46 +0000

    ulysse-common: add frameworks/native in vendorsetup
    - frameworks/native of nusantara cause blank, so repalce it.
    
    Change-Id: Ibdb7748fca8f186f118e0f6364f18111fb44cfd2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 gclang=prebuilts/clang/host/linux-x86/gabuters-clang
+native=vendor/nusantara
 
 if [ ! -d $gclang ]; then
 echo "Gabuters clang is empty! cloning.."
 git clone https://github.com/Gabuters-Dev/gabuters-clang $gclang
 else
 echo "Gabuters clang exist, skipping.."
+fi
+
+if [ ! -d $native ]; then
+echo "skipping.. This is not Nusantara-ROM"
+else
+echo "Building Nusantara-ROM.."
+rm -rf frameworks/native
+git clone https://github.com/mgs28-mh/frameworks_native -b 11_r48 frameworks/native
 fi
 
 git clone https://github.com/Gabuters-Dev/vendor_xiaomi_dirac -b part vendor/xiaomi/part
