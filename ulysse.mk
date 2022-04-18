6f4021ded8d55d4cd91672248a72019aef3aeb93
Quallenauge <Hamsi2k@freenet.de>
2022-04-18 04:58:17 +0000

    ulysse-common: Switch to aidl vibrator hal
    Change-Id: I4dec22449f33bdcd6170759075bdcec5bb715031

--- ulysse.mk
+++ ulysse.mk
@@ -461,8 +461,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
