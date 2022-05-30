de47540992e947396bc54f2f599ca36ca2b74016
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    ulysse-common: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- ulysse.mk
+++ ulysse.mk
@@ -196,8 +196,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey
 
 # Ebtables
