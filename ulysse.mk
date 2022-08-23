de82aaff5ef7d3c391fc8eeb13e455f01555290d
Josh Wu <joshwu@google.com>
2022-08-23 19:55:40 +0400

    ulysse-common: Switch Bt Audio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- ulysse.mk
+++ ulysse.mk
@@ -84,7 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.a2dp@1.0-service \
     android.hardware.soundtrigger@2.2-impl
